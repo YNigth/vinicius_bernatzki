798d4d56bb03fdaae16f2f8878fd515670367725
YNigth <145170100+YNigth@users.noreply.github.com>
2023-11-23 23:52:34 -0300

    trabalho completo feito com as mudanças
    tudo feito e comentado no index

--- TrabalhoSexta-main/scripts.js
+++ TrabalhoSexta-main/scripts.js
@@ -1,17 +1,15 @@
 const myButton = document.getElementById("my-button");
 
-// Variável para armazenar a cor do texto atual
 let textColor = "black";
 
-// Adiciona um evento de clique ao botão
+
 myButton.addEventListener("click", () => {
-    // Altera a cor do texto para a cor alternada
+
     textColor = textColor === "white" ? "black" : "white";
 
-    // Altera a cor do texto do botão
+
     myButton.style.color = textColor;
 
-    // Altera a cor do texto de todos os elementos de texto na página
     const allTextElements = document.querySelectorAll("*");
     for (const textElement of allTextElements) {
         textElement.style.color = textColor;
