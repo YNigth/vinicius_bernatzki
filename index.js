ca9fbb7690899bd47f614b14358b6d52d322afea
YNigth <145170100+YNigth@users.noreply.github.com>
2023-11-24 13:11:26 -0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-function getGameTitle(gameElement) {
-    return gameElement.querySelector('.game h3').textContent;
-  }
-  
-  const gameElement = document.querySelector('.games-list .game');
-const gameTitle = getGameTitle(gameElement);
-
-console.log(gameTitle);
